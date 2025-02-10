2011e84ec4f7738a3bbbf3d015cc0d800dc887c3
AaronWang7 <aaron.wang@ucas-edu.net>
2025-02-10 10:49:47 -0700

    note

---
+++ reading.py
@@ -0,0 +1,9 @@
+import csv 
+
+content = open('Notes_2nd/things.txt','r'.read())
+
+with open ("Notes_2nd/sample.csv", 'r')
+next(csv_reader)
+for row in csv_reader:
+    print(f'username{row[0]}, favort color')
+    
